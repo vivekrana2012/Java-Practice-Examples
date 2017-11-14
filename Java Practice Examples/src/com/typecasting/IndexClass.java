6ebbb2a366f44e96e0b58ac1f31ed940db61e6a2
Vivek Rana <vivekrana.2012@gmail.com>
2017-11-14 20:15:02 +0530

    added String and int conversions

--- Java Practice Examples/src/com/typecasting/IndexClass.java
+++ Java Practice Examples/src/com/typecasting/IndexClass.java
@@ -1,9 +1,20 @@
 package com.typecasting;
+import java.util.ArrayList;
 
 public class IndexClass {
 
     public static void main(String args[]){
-        ElderChild base = new ElderChild("Vivek", 25);
+
+        ArrayList mylist = new ArrayList();
+        mylist.add("Vivek");
+        mylist.add(25);
+
+        String name = (String) mylist.get(0);
+        String age = (String) mylist.get(1); // throws ClassCastException - cannot convert from int to string
+
+        BaseClass base = new ElderChild( (String)mylist.get(0), (int)mylist.get(1));
+        // requires type casting
+        ElderChild elderChild = (ElderChild) base;
         // base.age() not permitted
         base.getName();
     }
