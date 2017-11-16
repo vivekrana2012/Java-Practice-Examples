76c6dcafdafcaace53cc404b4f25d686c88abb3f
Vivek Rana <vivekrana.2012@gmail.com>
2017-11-16 22:38:46 +0530

    Added another hashmap example

--- Java Practice Examples/src/com/collectionexamples/CollectionExamples.java
+++ Java Practice Examples/src/com/collectionexamples/CollectionExamples.java
@@ -11,7 +11,12 @@ public class CollectionExamples {
         scores.put("rahul", "somevalue");
         scores.put("bhawna", "somevalue");
 
+        // hash code of vivek and null is same
+        System.out.println("vivek".hashCode());
+        System.out.println("rahul".hashCode());
         System.out.println(scores.get(null).hashCode());
+
+        // all 3 have same hashcode
         System.out.println(scores.get("vivek").hashCode());
         System.out.println(scores.get("rahul").hashCode());
         System.out.println(scores.get("bhawna").hashCode());
