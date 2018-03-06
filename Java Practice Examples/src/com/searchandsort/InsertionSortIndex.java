4474bae0358bbfda2eeaf6b0ad1264eb4c0ea446
Vivek Rana <vivekrana.2012@gmail.com>
2018-03-07 00:40:39 +0530

    Sort and Search

--- Java Practice Examples/src/com/searchandsort/InsertionSortIndex.java
+++ Java Practice Examples/src/com/searchandsort/InsertionSortIndex.java
@@ -15,9 +15,26 @@ public class InsertionSortIndex {
     }
 
     public static void InsertionSort(List<Integer> list){
+
+        int greaterThan;
+        int smallerThan;
+
         for(int counter = 0; counter < list.size(); counter++){
+
+            greaterThan = 0;
+            smallerThan = 0;
+
             for(int anotherCounter = counter; anotherCounter < 0; anotherCounter--){
-                //if(list.get(anotherCounter) < ){}
+                if(list.get(counter) < list.get(anotherCounter)){
+                    greaterThan = anotherCounter;
+                }else
+                    smallerThan = anotherCounter;
+
+                if(smallerThan == greaterThan-1){
+                    list.add(greaterThan, list.get(anotherCounter));
+                    list.remove(anotherCounter+1);
+                    break;
+                }
             }
         }
     }
